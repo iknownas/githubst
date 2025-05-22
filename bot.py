10a931b085d4c3ff9e412dcf703a68da48c71419
iknownas <tctarachand86@gmail.com>
2025-05-22 08:32:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,9 @@
-import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        "Welcome to Github Seller! Click below to view products:\n\n"
-        "https://githubst.netlify.app"
-    )
+    await update.message.reply_text("Welcome to Github Seller! Click below to view products:\n\nhttps://githubstudents.netlify.app/")
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = ApplicationBuilder().token("7664448568:AAFXPc3fbBKvoH8tNbKGU-DpqPJykAHks9k").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
